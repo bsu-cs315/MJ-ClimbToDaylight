a12fdd41abcf8bec4f4789acdd887fe14ac80eb2
Andrew <astafford2@bsu.edu>
2020-10-15 17:30:03 -0400

    Add more platforms and win line

--- project/src/Level-1.gd
+++ project/src/Level-1.gd
@@ -14,3 +14,8 @@ func _process(_delta):
 		if player.position.y > player.player_cam().get_camera_screen_center().y+352:
 			player.queue_free()
 			print("dead")
+
+
+func _on_WinLine_body_shape_entered(body_id, _body, _body_shape, _area_shape):
+	if body_id == player.get_instance_id():
+		print("win")
