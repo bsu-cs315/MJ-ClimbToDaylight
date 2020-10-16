50dcd77d4ba7eb71bd31c133c5aa14ee9aa5a9d5
Andrew <astafford2@bsu.edu>
2020-10-16 00:46:27 -0400

    Add game_over hud and show when dead
    also reload current scene on retry button pressed

--- project/src/Level-1.gd
+++ project/src/Level-1.gd
@@ -1,20 +1,37 @@
 extends Node2D
 
 
-onready var player = $Player
+onready var player := $Player
+onready var win_hud
+onready var game_over_hud := $GameOverHUD
 
 
-func _ready():
-	pass
+func kill_player():
+	player.queue_free()
+	set_correct_hud("dead")
+
+
+func win_player():
+	print("win")
+
+
+func set_correct_hud(player_condition):
+	if player_condition == "win":
+		pass
+	elif player_condition == "dead":
+		game_over_hud.visible = true
 
 
 func _process(_delta):
 	if player:
 		if player.position.y > player.player_cam().get_camera_screen_center().y+352:
-			player.queue_free()
-			print("dead")
+			kill_player()
 
 
 func _on_WinLine_body_shape_entered(body_id, _body, _body_shape, _area_shape):
 	if body_id == player.get_instance_id():
-		print("win")
+		win_player()
+
+
+func _on_RetryButton_pressed():
+	var _ignored = get_tree().reload_current_scene()
