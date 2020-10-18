d4ba447cd354df3a0486b05f4cef6c24da7e4739
Andrew <astafford2@bsu.edu>
2020-10-18 02:34:24 -0400

    Use remove_child to temp fix web bug

--- project/src/Book.gd
+++ project/src/Book.gd
@@ -5,7 +5,7 @@ var min_speed := 150 setget , min_speed
 var max_speed := 250 setget , max_speed
 
 func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+	remove_child(self)
 
 
 func min_speed():
