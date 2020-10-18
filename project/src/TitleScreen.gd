fafa884ae4687a2bdf0ecf685942bc8208c9f302
Andrew <astafford2@bsu.edu>
2020-10-18 01:35:25 -0400

    Build out level 2 with platforms to goal
    also added bonus level button to title screen

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var level1_scene_path := "res://project/src/Level-1.tscn"
+var bonus_level_path := "res://project/src/EndlessBooks.tscn"
 
 onready var instructions := $InstructionsPopup
 
@@ -12,3 +13,7 @@ func _on_InstructionsButton_pressed():
 
 func _on_StartButton_pressed():
 	var _ignored = get_tree().change_scene(level1_scene_path)
+
+
+func _on_BonusLevelButton_pressed():
+	var _ignored = get_tree().change_scene(bonus_level_path)
